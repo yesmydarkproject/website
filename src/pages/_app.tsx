d869124f29200d16107080390dc95b24fa408275
MaySoMusician <maysomusician@gmail.com>
2021-12-24 08:26:19 +0900

    feat: add initial screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { EmotionCache } from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
+import type { NextPage } from "next";
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
+import type { ReactElement, ReactNode } from "react";
 
 import defaultSEOConfig from "../../next-seo.config";
 import Layout from "components/layout";
@@ -14,8 +17,13 @@ import "styles/globals.css";
 
 const clientSideEmotionCache = createEmotionCache();
 
+type MyNextPage = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
+  Component: MyNextPage;
 }
 
 const MyApp = ({
@@ -23,6 +31,23 @@ const MyApp = ({
   pageProps,
   emotionCache = clientSideEmotionCache,
 }: MyAppProps) => {
+  const getLayout =
+    Component.getLayout ??
+    (() => (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    ));
+
+  useEffect(() => {
+    const setHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+    window.addEventListener("resize", setHeight);
+    setHeight();
+  });
+
   return (
     <CacheProvider value={emotionCache}>
       <ChakraProvider theme={customTheme}>
@@ -33,9 +58,7 @@ const MyApp = ({
           />
         </Head>
         <DefaultSeo {...defaultSEOConfig} />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        {getLayout(<Component {...pageProps} />)}
       </ChakraProvider>
     </CacheProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
 import { Box } from "@chakra-ui/react";
+import type { ReactElement } from "react";
+
+import Footer from "components/layout/Footer";
+// import Header from "components/layout/Header";
+import TheInitialScreen from "components/TheInitialScreen";
 
 const Home = () => {
   return (
     <Box mb={8} w="full">
       {/* */}
+      ddd
+    </Box>
+  );
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Box margin="0 auto" /* transition="0.5s ease-out" */>
+      <Box>
+        {/* <Header /> */}
+        <Box as="main">{page}</Box>
+        <Footer />
+      </Box>
+      <TheInitialScreen />
     </Box>
   );
 };
