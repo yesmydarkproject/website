8b19822e90c706930731d90eb99f67072a930ee9
MaySoMusician <maysomusician@gmail.com>
2021-12-31 07:18:34 +0900

    fix: file path of svg for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -64,7 +64,7 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#32243e" />
 
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
