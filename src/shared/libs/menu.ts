cd096a954e416478e4bf4789c68e1376c69433d1
MaySoMusician <maysomusician@gmail.com>
2022-01-04 03:14:14 +0900

    fix: typo

--- src/shared/libs/menu.ts
+++ src/shared/libs/menu.ts
@@ -1,13 +1,13 @@
 export interface NavItem {
   text: string;
   href: string;
-  group: "OFFICIAL" | "FUN";
+  group: "OFFICIAL" | "FAN";
 }
 export const navItems: NavItem[] = [
   { text: "La+Profile", href: "/profile", group: "OFFICIAL" },
   { text: "La+News", href: "/news", group: "OFFICIAL" },
   { text: "La+Archives", href: "/archives", group: "OFFICIAL" },
   { text: "La+Works", href: "/works", group: "OFFICIAL" },
-  { text: "+mate ARTS", href: "/fan/arts", group: "FUN" },
-  { text: "+mate LINKS", href: "/fan/links", group: "FUN" },
+  { text: "+mate ARTS", href: "/fan/arts", group: "FAN" },
+  { text: "+mate LINKS", href: "/fan/links", group: "FAN" },
 ];
