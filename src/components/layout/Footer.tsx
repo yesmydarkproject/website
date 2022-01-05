2494f7253042125d67c43b59e4648a2b4eb66c47
MaySoMusician <maysomusician@gmail.com>
2022-01-06 01:29:25 +0900

    feat: add disclaimers to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,14 +8,29 @@ const Footer = () => {
       align="center"
       justify="center"
       pb={2}
+      px={2}
       position="sticky"
       top="100vh"
+      direction="column"
+      fontSize="0.875rem"
+      lineHeight="120%"
+      textAlign="center"
     >
       <Text>
         &copy;&ensp;2021&ensp;YesMyDark Project&ensp;&ndash;&ensp;
         <Link href="https://github.com/yesmydarkproject/website" isExternal>
           GitHub
         </Link>
+      </Text>
+      <Text mt={1} fontSize="0.8rem" fontWeight={300}>
+        本サイトはカバー株式会社・ホロライブプロダクションとの提携関係、両組織からの承認、両組織への所属は一切ありません。
+      </Text>
+      <Text fontSize="0.8rem" fontWeight={300}>
+        使用しているコンテンツ
+        <Text as="span" fontSize="88%">
+          （イラストやテキストなどを含むがこれに限らない）
+        </Text>
+        の著作権は、正当な権利を有する第三者に帰属します。
       </Text>
     </Flex>
   );
