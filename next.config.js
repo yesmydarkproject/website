3b5d61d3b2432ce0043a8f830a386134abe25dff
MaySoMusician <maysomusician@gmail.com>
2022-10-01 02:43:41 +0000

    chore: enable hot reload in vm

--- next.config.js
+++ next.config.js
@@ -8,11 +8,18 @@ module.exports = withPWA({
       process.env.NODE_ENV === "development" ||
       process.env.NODE_ENV === "preview" ||
       process.env.NODE_ENV === "production",
-      // delete two lines above to enable PWA in production deployment
-      // add your own icons to public/manifest.json 
-      // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+    // delete two lines above to enable PWA in production deployment
+    // add your own icons to public/manifest.json
+    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
     dest: "public",
     register: true,
   },
   reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 });
