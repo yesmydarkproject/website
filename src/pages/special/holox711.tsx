69609f2aa6207d8c1ebe5d2a550d951f0e5ce9d5
MaySoMusician <maysomusician@gmail.com>
2022-10-01 07:12:50 +0000

    feat(special/holox711): add page description

--- src/pages/special/holox711.tsx
+++ src/pages/special/holox711.tsx
@@ -3,9 +3,10 @@ import { NextSeo } from "next-seo";
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
-const pageTitle = "holoX セブンイレブンコラボ";
+const pageTitle = "holoX セブンイレブンコラボの対象店舗を地図で見る";
 const url = "https://yesmydark.com/archives";
-
+const description =
+  "holoXとセブンのコラボの対象店舗一覧がとんでもないことになってたので作りました。普段は総帥の非公式ファンサイトです。×××「刮目せよ！ 吾輩の名前は……ラプラス・ダークネスだ！！」 ホロライブ6期生／秘密結社holoX総帥 ラプラス・ダークネスの非公式ファンサイト。 Yes My Dark!!";
 const Archives = () => {
   const Map = useMemo(
     () =>
@@ -20,9 +21,11 @@ const Archives = () => {
       <NextSeo
         title={pageTitle}
         canonical={url}
+        description={description}
         openGraph={{
           url,
           title: pageTitle,
+          description,
         }}
       />
       <Center minH="30vh" placeItems="start">
