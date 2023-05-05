8d0e0c4b3eb6dcbcc316d5061126f0222f94beba
MaySoMusician <maysomusician@gmail.com>
2023-05-05 07:16:38 +0000

    refactor(deps): update Chakra UI to V2

--- src/components/motion/Box.tsx
+++ src/components/motion/Box.tsx
@@ -1,10 +1,6 @@
-import { chakra, HTMLChakraProps } from "@chakra-ui/react";
-import { motion, HTMLMotionProps } from "framer-motion";
+import { chakra } from "@chakra-ui/react";
+import { motion, isValidMotionProp } from "framer-motion";
 
-import { Merge } from "types/merge";
+const ChakraBox = chakra(motion.div, { shouldForwardProp: isValidMotionProp });
 
-type MotionBoxProps = Merge<HTMLChakraProps<"div">, HTMLMotionProps<"div">>;
-
-const MotionBox: React.FC<MotionBoxProps> = motion(chakra.div);
-
-export default MotionBox;
+export default ChakraBox;
