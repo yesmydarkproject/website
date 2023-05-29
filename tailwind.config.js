ee7ef2aedfe50b8453b81a1673c8ef803a094def
MaySoMusician <maysomusician@gmail.com>
2023-05-29 13:22:59 +0000

    fix: update Tailwind's breakpoints config per Chakra UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "30em", // 480px
+      md: "48em", // 768px
+      lg: "62em", // 992px
+      xl: "80em", // 1280px
+      "2xl": "96em", // 1536px
+    },
   },
   plugins: [],
 };
